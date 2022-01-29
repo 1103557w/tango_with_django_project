f9ac2c42953a2858d3a78ff4baa64dcec23c92d0
Angus Wilson <1103557w@student.gla.ac.uk>
2022-01-29 14:20:31 +0000

    Updated to pass C4 tests, small changes such as removing some newlines that didn't match the expected code and adding a picture of rango (blini cat) to my about page (which already contains another picture of blini cat)

--- rango/views.py
+++ rango/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    context_dict = {'boldmessage': 'Crunchy, creamy cookie, candy, cupcake!'}
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
     # returns rendered response to client, with the template we've set up
     # and the context dict
     return render(request, 'rango/index.html', context=context_dict)
