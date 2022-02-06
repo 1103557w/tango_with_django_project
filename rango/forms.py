e564eff940e9dca3e92494428f82018a62165eb6
Angus Wilson <1103557w@student.gla.ac.uk>
2022-02-06 12:43:27 +0000

    Added ability to add pages, failing tests presently so will need to figure out why that is

--- rango/forms.py
+++ rango/forms.py
@@ -1,12 +1,15 @@
 from django import forms
 from rango.models import Page, Category
 
+initialVal = 0
+maxLength = 128
+
 
 class CategoryForm(forms.ModelForm):
     name = forms.CharField(max_length=128, help_text="Please enter the category name.")
-    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
-    likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
-    slug = forms.CharField(widget=forms.HiddenInput(), initial=0)
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=initialVal)
+    likes = forms.IntegerField(widget=forms.HiddenInput(), initial=initialVal)
+    slug = forms.CharField(widget=forms.HiddenInput(), initial=initialVal)
 
     class Meta:
         # provides assosciation between the ModelForm and a model
@@ -16,9 +19,11 @@ class CategoryForm(forms.ModelForm):
 
 class PageForm(forms.ModelForm):
     title = forms.CharField(
-        max_length=128, help_text="Please enter the title of the page."
+        max_length=maxLength, help_text="Please enter the title of the page."
     )
-    url = forms.URLField(max_length=128, help_text="Please enter the url of the page.")
+    url = forms.URLField(
+        max_length=maxLength, help_text="Please enter the url of the page."
+    )
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
     class Meta:
@@ -26,3 +31,17 @@ class PageForm(forms.ModelForm):
         model = Page
 
         exclude = ("category",)
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get("url")
+
+        # checks if url is empty and starts with http or https
+        # slight diversion from the book for now as I will be
+        # prepending https as it's better!
+        if url and not url.startswith("https://"):
+            if not url.startswith("http://"):
+                url = f"https://{url}"
+                cleaned_data["url"] = url
+
+        return cleaned_data
